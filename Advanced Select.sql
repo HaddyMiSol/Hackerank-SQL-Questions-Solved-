d9ace407ceafc1355b2fcea9b623c65d06168371
Oluwabukola Arowolo <arowolooluwabukola14@gmail.com>
2025-03-08 00:06:49 +0100

    Update Advanced Select.sql

--- Advanced Select.sql
+++ Advanced Select.sql
@@ -18,6 +18,7 @@ from
 (select count(*) as occupation_count,occupation from OCCUPATIONS
 group by occupation)a
 order by occupation_count,occupation 
+  
 
 
 --Occupations
@@ -57,5 +58,15 @@ inner join lead_manager l on c.company_code = l.company_code
 inner join senior_manager s on l.lead_manager_code = s.lead_manager_code
 inner join manager m on s.senior_manager_code = m.senior_manager_code
 inner join employee e on m.manager_code = e.manager_code
+group by c.company_code, c.founder
+order by c.company_code asc
+
+--OR (Since company_code is a primary key in all the tables)
+
+select c.company_code, c.founder, count(distinct(l.lead_manager_code)), count(distinct(s.senior_manager_code)), count(distinct(m.manager_code)), count(distinct(e.employee_code)) from company c 
+inner join lead_manager l on c.company_code = l.company_code
+inner join senior_manager s on l.company_code = s.company_code
+inner join manager m on s.company_code = m.company_code
+inner join employee e on m.company_code = e.company_code
 group by c.company_code, c.founder
 order by c.company_code asc
