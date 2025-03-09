f552595e340edcb88e240424afa24b19ff63b70b
Oluwabukola Arowolo <arowolooluwabukola14@gmail.com>
2025-03-09 11:57:56 +0100

    Update Basic Join.sql

--- Basic Join.sql
+++ Basic Join.sql
@@ -48,3 +48,14 @@ inner join (select s.hacker_id, max(score) as sscore from Submissions s group by
 group by h.hacker_id, h.name
 having sum(sscore) > 0
 order by sum(sscore) desc, h.hacker_id asc
+
+
+--Challenges
+select h.hacker_id, h.name, count(*) from Hackers h inner join Challenges c on h.hacker_id=c.hacker_id
+where h.hacker_id not in 
+(select t1.hacker_id
+from(select hacker_id, count(*) as cnt from Challenges group by hacker_id) as t1 
+inner join 
+(select hacker_id, count(*) as cnt from Challenges group by hacker_id) as t2 on t1.hacker_id!=t2.hacker_id and t1.cnt=t2.cnt and t1.cnt != (select count(*) from Challenges group by hacker_id order by count(*) desc limit 1))
+group by h.hacker_id, h.name
+order by count(*) desc, h.hacker_id
